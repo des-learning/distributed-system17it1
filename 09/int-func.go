2dc692acb48a4af3b071b0a5fdb4d189d7408bea
Des Dulianto <des.dulianto@bukalapak.com>
2020-03-04 20:55:14 +0700

    map, reduce

--- 09/int-func.go
+++ 09/int-func.go
@@ -1,16 +1,18 @@
 package main
 
+import "fmt"
+
 func IntRange(start int, end int, delta int) <-chan int {
 	out := make(chan int, 1)
 	switch {
-	case delta >= 0:
+	case start <= end && delta >= 0:
 		go func() {
 			for i := start; i < end; i += delta {
 				out <- i
 			}
 			close(out)
 		}()
-	case delta < 0:
+	case start >= end && delta < 0:
 		go func() {
 			for i := start; i > end; i += delta {
 				out <- i
@@ -49,6 +51,25 @@ func IntFilter(numbers <-chan int, fun func(int) bool) <-chan int {
 	return out
 }
 
+func IntMap(numbers <-chan int, fun func(int) int) <-chan int {
+	out := make(chan int, 1)
+	go func() {
+		for i := range numbers {
+			out <- fun(i)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func IntReduce(start int, numbers <-chan int, fun func(int, int) int) int {
+	result := start
+	for i := range numbers {
+		result = fun(result, i)
+	}
+	return result
+}
+
 func main() {
 	/*for i := range IntRange(1, 100, 1) {
 		fmt.Println(i)
@@ -59,8 +80,19 @@ func main() {
 	/*for i := range IntSlice([]int{1, 2, 3, 4, 5, 6, 7}) {
 		fmt.Println(i)
 	}*/
-	/*for i := range IntFilter(IntSlice([]int{1, 2, 3, 4, 5, 6, 7, 8}),
-		func(x int) bool { return x%2 == 0 }) {
+	satuSampai999 := IntRange(1, 1000, 1)
+	ganjil := func(x int) bool { return x%2 != 0 }
+	genap := func(x int) bool { return x%2 == 0 }
+	/*for i := range IntFilter(satuSampai999, ganjil) {
 		fmt.Println(i)
 	}*/
+	/*kali3 := func(x int) int { return x * 3 }
+	for i := range IntMap(satuSampai999, kali3) {
+		fmt.Println(i)
+	}*/
+	sum := func(x int, y int) int { return x + y }
+	//fmt.Println(IntReduce(0, satuSampai999, sum))
+	fmt.Println(IntReduce(0, IntFilter(satuSampai999, ganjil), sum))
+	satuSampai999 = IntRange(1, 1000, 1)
+	fmt.Println(IntReduce(0, IntFilter(satuSampai999, genap), sum))
 }
